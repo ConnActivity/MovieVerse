0a141df25d432c5b2a2c82a7d6529dae18c61ca9
Segelente <marion@fauxia.de>
2023-11-26 19:55:03 +0100

    changed back to for loop way

--- script.py
+++ script.py
@@ -1,22 +1,17 @@
 import downloader
-# import insert
+
+import insert
 
 all_movie_ids = set()
 all_movies = []
-PAGES = 10
-
+PAGES = 2
 for i in range(1, PAGES):
-    all_movie_ids.update(
-        movie.id
-        for movie_list in [
-            downloader.get_popular_movies(i).results,
-            downloader.get_top_rated_movies(i).results,
-            downloader.get_now_playing_movies(i).results,
-        ]
-        for movie in movie_list
-    )
-
-for movie_id in all_movie_ids:
-    all_movies.append(downloader.get_movie_details(movie_id))
-print(len(all_movies))
+    for movie in downloader.get_popular_movies(i).results:
+        all_movie_ids.add(movie.id)
+    for movie in downloader.get_top_rated_movies(i).results:
+        all_movie_ids.add(movie.id)
+    for movie in downloader.get_now_playing_movies(i).results:
+        all_movie_ids.add(movie.id)
+for movie_ids in all_movie_ids:
+    all_movies.append(downloader.get_movie_details(movie_ids))
 # insert.insert_movies(all_movies)
