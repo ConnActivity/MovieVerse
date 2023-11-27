1acdb1b6cea9279faa88639b80a09eaa568715d8
mfloto <60036186+mfloto@users.noreply.github.com>
2023-11-27 20:01:28 +0100

    feat: add try catch

--- script.py
+++ script.py
@@ -1,13 +1,16 @@
-import downloader
+import time
 
+import downloader
 import insert
 
 all_movie_ids = set()
 all_movies = []
-all_changes_ids = set()
 all_changes = []
-PAGES = 2
-for i in range(1, PAGES):
+PAGES = 1000
+
+start_time = time.time()
+for i in range(1, PAGES + 1):
+    print(f"Page {i} of {PAGES} started")
     for movie in downloader.get_popular_movies(i).results:
         all_movie_ids.add(movie.id)
     for movie in downloader.get_top_rated_movies(i).results:
@@ -18,18 +21,50 @@ for i in range(1, PAGES):
         all_movie_ids.add(changes["id"])
         print(changes["id"])
         all_changes.append(changes)
+    print(f"Page {i} of {PAGES} finished")
+
+end_time = time.time()
+print(f"Iterating through pages took {end_time - start_time} seconds to complete")
 
 all_movie_ids = {x for x in all_movie_ids if x is not None}
 
-for movie_ids in all_movie_ids:
-    print(movie_ids)
-    all_movies.append(downloader.get_movie_details(movie_ids))
+start_time = time.time()
+print("Downloading movie details")
+for i, movie_ids in enumerate(all_movie_ids):
+    try:
+        all_movies.append(downloader.get_movie_details(movie_ids))
+    except Exception as e:
+        print(f"Could not load movie with id {movie_ids}")
+        print(e)
+    if i % 25 == 0:
+        print(f"{i} of {len(all_movie_ids)} loaded from API")
 
-print(len(all_movies))
+end_time = time.time()
+print(f"Downloading details took {end_time - start_time} seconds to complete")
 
-for movie in all_movies:
-    insert.insert_movie(movie)
+start_time = time.time()
+print("Inserting movies into database")
+for i, movie in enumerate(all_movies):
+    try:
+        insert.insert_movie(movie)
+    except Exception as e:
+        print(f"Could not insert movie with id {movie.id}")
+        print(e)
+    if i % 25 == 0:
+        print(f"{i} of {len(all_movies)} inserted into database")
 
+end_time = time.time()
+print(f"Inserting movies took {end_time - start_time} seconds to complete")
+
+start_time = time.time()
+print("Inserting changes into database")
 for changes in all_changes:
     for key, value in changes["changes"].items():
-        insert.insert_movie_change(changes["id"], key, value)
+        try:
+            insert.insert_movie_change(changes["id"], key, value)
+        except Exception as e:
+            print(f"Could not insert change for movie with id {changes['id']}")
+            print(e)
+
+end_time = time.time()
+print(f"Inserting changes took {end_time - start_time} seconds to complete")
