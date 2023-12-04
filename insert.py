28dfce43a89dd1186dfcb7fd61cb31e7ec9eff08
mfloto <60036186+mfloto@users.noreply.github.com>
2023-12-04 14:58:49 +0100

    feat: Add bulk insert for countries, companies, languages

--- insert.py
+++ insert.py
@@ -6,7 +6,6 @@ from themoviedb.schemas import Movie, Person, Genre, Company
 from themoviedb.schemas.countries import Country
 from themoviedb.schemas.languages import Language
 
-url = f"postgresql://postgres:postgres@49.13.1.33:5333/movie_db"
 
 class MoviePopularity(NamedTuple):
     movie_id: int
@@ -23,6 +22,21 @@ class MovieChange(NamedTuple):
 class MovieGenre(NamedTuple):
     movie_id: int
     genre_id: int
+
+
+class MovieProductionCompany(NamedTuple):
+    movie_id: int
+    production_company_id: int
+
+
+class MovieProductionCountry(NamedTuple):
+    movie_id: int
+    iso_3166_1: str
+
+
+class MovieSpokenLanguage(NamedTuple):
+    movie_id: int
+    iso_639_1: str
 
 
 conn = psycopg2.connect(url)
@@ -44,21 +58,23 @@ def insert_movie(movies: List[Movie]):
     cursor.executemany(sql, movies_data)
     conn.commit()
 
+    # TODO: Should be faster without list comprehension when iterating over movies once
     insert_movie_popularity([MoviePopularity(movie.id, movie.popularity, movie.vote_average) for movie in movies])
     insert_genre([genre for movie in movies for genre in movie.genres])
-    insert_movie_genres([(movie.id, genre.id) for movie in movies for genre in movie.genres])
+    insert_movie_genres([MovieGenre(movie.id, genre.id) for movie in movies for genre in movie.genres])
 
-    for company in movie.production_companies:
-        insert_production_company(company)
-        insert_movie_production_companies(movie.id, company.id)
+    insert_production_company([company for movie in movies for company in movie.production_companies])
+    insert_movie_production_companies(
+        [MovieProductionCompany(movie.id, company.id) for movie in movies for company in movie.production_companies])
 
-    for country in movie.production_countries:
-        insert_country(country)
-        insert_movie_production_countries(movie.id, country.iso_3166_1)
+    insert_country([country for movie in movies for country in movie.production_countries])
+    insert_movie_production_countries(
+        [MovieProductionCountry(movie.id, country.iso_3166_1) for movie in movies for country in
+         movie.production_countries])
 
-    for language in movie.spoken_languages:
-        insert_spoken_language(language)
-        insert_movie_spoken_languages(movie.id, language.iso_639_1)
+    insert_spoken_language([language for movie in movies for language in movie.spoken_languages])
+    insert_movie_spoken_languages(
+        [MovieSpokenLanguage(movie.id, language.iso_639_1) for movie in movies for language in movie.spoken_languages])
 
 
 def insert_movie_popularity(popularity_data: List[MoviePopularity]):
@@ -93,91 +109,88 @@ def insert_genre(genres: List[Genre]):
             VALUES 
             (%s, %s) ON CONFLICT (id) DO NOTHING"""
 
-    cursor.executemany(sql, genres)
+    cursor.executemany(sql, [(genre.id, genre.name) for genre in genres])
     conn.commit()
 
 
-def insert_movie_genres(movie_genres: List[Tuple[int, int]]):
+def insert_movie_genres(movie_genres: List[MovieGenre]):
     sql = """INSERT INTO moviegenres 
             (movie_id, genre_id) 
             VALUES 
             (%s, %s) ON CONFLICT (movie_id, genre_id) DO NOTHING"""
 
-    cursor.execute(sql, movie_genres)
+    cursor.executemany(sql, movie_genres)
     conn.commit()
 
 
-def insert_production_company(company: Company):
+def insert_production_company(companies: List[Company]):
     sql = """INSERT INTO productioncompanies 
             (id, name, logo_path, origin_country) 
             VALUES 
             (%s, %s, %s, %s) ON CONFLICT (id) DO NOTHING"""
 
-    production_company_data = (company.id, company.name, company.logo_path, company.origin_country)
+    production_company_data = [(company.id, company.name, company.logo_path, company.origin_country) for company in
+                               companies]
 
-    cursor.execute(sql, production_company_data)
+    cursor.executemany(sql, production_company_data)
     conn.commit()
 
 
-def insert_movie_production_companies(movie_id: int, production_company_id: int):
+def insert_movie_production_companies(companies: List[MovieProductionCompany]):
     sql = """INSERT INTO movieproductioncompanies 
             (movie_id, production_company_id) 
             VALUES 
             (%s, %s) ON CONFLICT (movie_id, production_company_id) DO NOTHING"""
 
-    movie_production_company_data = (movie_id, production_company_id)
-
-    cursor.execute(sql, movie_production_company_data)
+    cursor.executemany(sql, companies)
     conn.commit()
 
 
-def insert_country(region: Country):
+def insert_country(region: List[Country]):
     sql = """INSERT INTO productioncountries 
             (iso_3166_1, name) 
             VALUES 
             (%s, %s) ON CONFLICT (iso_3166_1) DO NOTHING"""
 
-    region_data = (region.iso_3166_1, region.name)
+    region_data = [(region.iso_3166_1, region.name) for region in region]
 
-    cursor.execute(sql, region_data)
+    cursor.executemany(sql, region_data)
     conn.commit()
 
 
-def insert_movie_production_countries(movie_id: int, country_code: str):
+def insert_movie_production_countries(countries: List[MovieProductionCountry]):
     # country code follows ISO 3166-1
     sql = """INSERT INTO movieproductioncountries 
             (movie_id, iso_3166_1) 
             VALUES 
             (%s, %s) ON CONFLICT (movie_id, iso_3166_1) DO NOTHING"""
 
-    movie_production_country_data = (movie_id, country_code)
-
-    cursor.execute(sql, movie_production_country_data)
+    cursor.executemany(sql, countries)
     conn.commit()
 
 
-def insert_spoken_language(language: Language):
+def insert_spoken_language(languages: List[Language]):
     sql = """INSERT INTO spokenlanguages 
             (iso_639_1, name, english_name) 
             VALUES 
             (%s, %s, %s) ON CONFLICT (iso_639_1) DO NOTHING"""
 
-    language_data = (language.iso_639_1, language.name, language.english_name)
+    language_data = [(language.iso_639_1, language.name, language.english_name) for language in languages]
 
-    cursor.execute(sql, language_data)
+    cursor.executemany(sql, language_data)
     conn.commit()
 
 
-def insert_movie_spoken_languages(movie_id: int, language_code: str):
+def insert_movie_spoken_languages(languages: List[MovieSpokenLanguage]):
     # language code follows ISO 639-1
     sql = """INSERT INTO moviespokenlanguages 
             (movie_id, iso_639_1) 
             VALUES 
             (%s, %s) ON CONFLICT (movie_id, iso_639_1) DO NOTHING"""
 
-    movie_spoken_language_data = (movie_id, language_code)
+    movie_spoken_language_data = [(language.movie_id, language.iso_639_1) for language in languages]
 
-    cursor.execute(sql, movie_spoken_language_data)
+    cursor.executemany(sql, movie_spoken_language_data)
     conn.commit()
 
 
