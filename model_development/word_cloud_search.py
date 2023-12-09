51427412f8e034a26673913c3101e7fe5d0c4836
Flippchen <91947480+Flippchen@users.noreply.github.com>
2023-12-09 18:48:31 +0100

    feature: Reduce dimensions and Plot

--- model_development/word_cloud_search.py
+++ model_development/word_cloud_search.py
@@ -57,3 +57,76 @@ else:
         pickle.dump(corpus_embeddings, file)
 
 
+def reduce_dimensions_and_plot(query_embedding, top_n, additional_k):
+    # Indices of the top n similar movies
+    distances = scipy.spatial.distance.cdist([query_embedding], corpus_embeddings, "cosine")[0]
+
+    results = zip(range(len(distances)), distances)
+    results = sorted(results, key=lambda x: x[1])
+
+    # Queried movie(s)
+    query_id = results[0:1][0][0]
+
+    # Correctly extract top_n_indices and additional_indices
+    top_n_indices = [idx for idx, _ in results[1:top_n + 1]]
+    additional_indices = [idx for idx, _ in results[top_n + 1:top_n + additional_k + 1]]
+
+    # Combine indices and extract corresponding embeddings
+    combined_indices = np.concatenate(([query_id], top_n_indices, additional_indices))  # 0 is for the queried movie
+    combined_embeddings = np.vstack((query_embedding, corpus_embeddings[combined_indices]))
+
+    # Reduce dimensions
+    tsne = TSNE(n_components=3, random_state=0)
+    reduced_embeddings = tsne.fit_transform(combined_embeddings)
+
+    # Extract titles for the plot
+    titles = [corpus[i] for i in combined_indices]
+
+    # Plotting with Plotly
+    trace_query = go.Scatter3d(
+        x=[reduced_embeddings[0, 0]],
+        y=[reduced_embeddings[0, 1]],
+        z=[reduced_embeddings[0, 2]],
+        mode='markers+text',
+        marker=dict(size=10, color='purple'),
+        name='Queried Movie',
+        text=[titles[0]],
+        textposition='top center'
+    )
+
+    trace_similar = go.Scatter3d(
+        x=reduced_embeddings[1:top_n + 1, 0],
+        y=reduced_embeddings[1:top_n + 1, 1],
+        z=reduced_embeddings[1:top_n + 1, 2],
+        mode='markers+text',
+        marker=dict(size=8, color='red'),
+        name=f'{top_n} Similar Movies',
+        text=titles[1:top_n + 1],
+        textposition='top center'
+    )
+
+    trace_additional = go.Scatter3d(
+        x=reduced_embeddings[top_n + 1:, 0],
+        y=reduced_embeddings[top_n + 1:, 1],
+        z=reduced_embeddings[top_n + 1:, 2],
+        mode='markers+text',
+        marker=dict(size=6, color='blue'),
+        name='Additional Movies',
+        text=titles[top_n + 1:],
+        textposition='top center'
+    )
+
+    layout = go.Layout(
+        margin=dict(l=0, r=0, b=0, t=0),
+        scene=dict(
+            xaxis=dict(title='TSNE 1'),
+            yaxis=dict(title='TSNE 2'),
+            zaxis=dict(title='TSNE 3')
+        )
+    )
+
+    fig = go.Figure(data=[trace_query, trace_similar, trace_additional], layout=layout)
+
+    return fig
+
+
