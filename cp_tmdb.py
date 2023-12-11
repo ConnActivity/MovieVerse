9bf25067a01d5f6322ff65e509ec4e5832e0cfd0
mfloto <60036186+mfloto@users.noreply.github.com>
2023-12-11 17:23:41 +0100

    feat: Aggregate movies to one pickle file

--- cp_tmdb.py
+++ cp_tmdb.py
@@ -11,7 +11,7 @@ MOVIE_ID_LIMIT = 1250000
 ROOT_PATH = "/media/maxi/mfloto_ext"
 
 
-def download_movie_details(movie_id):
+def download_movie_details(movie_id: int):
     if os.path.exists(f'{ROOT_PATH}/{movie_id}.pkl'):
         return
     try:
@@ -28,7 +28,7 @@ def download_all_movie_details():
                          for movie_id in range(31700, MOVIE_ID_LIMIT + 1)}
 
 
-def insert_movie_details(directory):
+def insert_movie_details(directory: str):
     all_movies = []
     for filename in os.listdir(directory):
         if filename.endswith(".pkl"):
@@ -39,9 +39,24 @@ def insert_movie_details(directory):
     print(f"Inserting {len(all_movies)} movies into database")
     insert.insert_movie(all_movies)
 
+
+def one_pickle(directory: str):
+    all_movies = []
+    for filename in os.listdir(directory):
+        if filename.endswith(".pkl"):
+            file_path = os.path.join(directory, filename)
+            with open(file_path, 'rb') as f:
+                movie = pickle.load(f)
+                all_movies.append(movie)
+    with open(directory + '/all_movies.pkl', 'wb') as f:
+        movie = pickle.dump(all_movies, f)
+        print(movie)
+
+
 if __name__ == '__main__':
     #download_all_movie_details()
-    time_start = time.time()
-    insert_movie_details(ROOT_PATH + "/all_movies_tbdm")
-    time_end = time.time()
-    print(f"Inserting movies took {time_end - time_start} seconds to complete")
+    #time_start = time.time()
+    #insert_movie_details(ROOT_PATH + "/all_movies_tbdm")
+    #time_end = time.time()
+    #print(f"Inserting movies took {time_end - time_start} seconds to complete")
+    one_pickle(ROOT_PATH + "/all_movies_tmdb")
